2b7b2841a6eaff4922f1a8bdf64994792dd37a71
jdeboi <jdeboi@gmail.com>
2017-08-29 01:26:45 -0500

    trying to fix butterly

--- music/sketches/butterfly.js
+++ music/sketches/butterfly.js
@@ -4,102 +4,146 @@ var moveStep = 0;
 var moveD = 1;
 var mode = 0;
 var currSelected = false;
-var bScale = .5;
+var bScale = .4;
+var movingApart = true;
+var audio;
 
 function preload() {
   backgroundImg = loadImage("assets/delta/background.jpg");
   bScale = map(windowWidth, 345, 1500, 345/1500*.5, .5);
-  butterflyParts.body[0] = new ButterflyPart(loadImage("assets/delta/butterfly_body.png"), windowWidth/2, windowHeight/2, false);
-  butterflyParts.left[0] = new ButterflyPart(loadImage("assets/delta/bottomLeft.png"), windowWidth/2-120*bScale*2, windowHeight/2+30*bScale*2, false);
-  butterflyParts.left[1] = new ButterflyPart(loadImage("assets/delta/topLeft.png"), windowWidth/2-150*bScale*2, windowHeight/2-150*bScale*2, false);
-  butterflyParts.left[2] = new ButterflyPart(loadImage("assets/delta/topLeft_0.png"), windowWidth/2-280*bScale*2, windowHeight/2-300*bScale*2, false);
-  butterflyParts.left[3] = new ButterflyPart(loadImage("assets/delta/middleLeft.png"), windowWidth/2-190*bScale*2, windowHeight/2+100*bScale*2, false);
+  butterflyParts.body[0] = new ButterflyPart(loadImage("assets/delta/butterfly_body.png"), -150, -450, false);
+  butterflyParts.left[0] = new ButterflyPart(loadImage("assets/delta/bottomLeft.png"), -420, -30, true);
+  butterflyParts.left[1] = new ButterflyPart(loadImage("assets/delta/topLeft.png"), -730, -180*3, true);
+  butterflyParts.left[2] = new ButterflyPart(loadImage("assets/delta/topLeft_0.png"),-700, -550, true);
+  //butterflyParts.left[3] = new ButterflyPart(loadImage("assets/delta/middleLeft.png"), -190*bScale*2, 100*bScale*2, true);
 
-  butterflyParts.right[0] = new ButterflyPart(loadImage("assets/delta/bottomRight.png"), windowWidth/2+150*bScale*2, windowHeight/2+30*bScale*2, true);
-  butterflyParts.right[1] = new ButterflyPart(loadImage("assets/delta/bottomRight_0.png"), windowWidth/2+190*bScale*2, windowHeight/2+100*bScale*2, true);
-  butterflyParts.right[2] = new ButterflyPart(loadImage("assets/delta/topRight.png"), windowWidth/2+130*bScale*2, windowHeight/2-150*bScale*2, true);
-  butterflyParts.right[3] = new ButterflyPart(loadImage("assets/delta/topRight_0.png"), windowWidth/2+210*bScale*2, windowHeight/2-280*bScale*2, true);
-  butterflyParts.right[4] = new ButterflyPart(loadImage("assets/delta/topRight_1.png"), windowWidth/2+250*bScale*2, windowHeight/2-300*bScale*2, true);
+  butterflyParts.right[0] = new ButterflyPart(loadImage("assets/delta/bottomRight.png"), 10, -20, true);
+  butterflyParts.right[1] = new ButterflyPart(loadImage("assets/delta/bottomRight_0.png"), 250, 200, true);
+  butterflyParts.right[2] = new ButterflyPart(loadImage("assets/delta/topRight.png"), 10, -175*3, true);
+  butterflyParts.right[3] = new ButterflyPart(loadImage("assets/delta/topRight_0.png"), 300, -650, true);
+  butterflyParts.right[4] = new ButterflyPart(loadImage("assets/delta/topRight_1.png"), 450, -580, true);
   //butterflyParts.right[5] = new ButterflyPart(loadImage("assets/delta/middleRight.png"), windowWidth/2+280, windowHeight/2, true);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  butterflyParts.body[0].resizePart(bScale);
+  for (var i = 0; i < butterflyParts.left.length; i++) {
+    butterflyParts.left[i].resizePart(bScale);
+  }
+
+  for (var i = 0; i < butterflyParts.right.length; i++) {
+    butterflyParts.right[i].resizePart(bScale);
+  }
 }
 
 
 
 function draw() {
-  if (millis() > 10000) mode = 1;
-  imageMode(CORNER);
-  background(backgroundImg);
-  imageMode(CENTER);
+  //if (movingApart) {
+  background(0);
+  //background(backgroundImg);
 
   // left wing
-  moveStep+= 1 * moveD;
+  // moveStep+= 1 * moveD;
+  //
+  // if (moveStep > 200) moveD = -1;
+  // else if (moveStep < 0) moveD = 1;
 
-  if (moveStep > 200) moveD = -1;
-  else if (moveStep < 0) moveD = 1;
+  //translate(0, 100);
 
-  translate(0, 100);
   for (var i = 0; i < butterflyParts.left.length; i++) {
     butterflyParts.left[i].display();
+    butterflyParts.left[i].mouseOver();
     butterflyParts.left[i].move();
     //butterflyParts.left[i].update(moveStep);
   }
 
   for (var i = 0; i < butterflyParts.right.length; i++) {
     butterflyParts.right[i].display();
+    butterflyParts.right[i].mouseOver();
     butterflyParts.right[i].move();
     //butterflyParts.right[i].update(moveStep);
   }
-
+  butterflyParts.body[0].display();
   // body
-  imageMode(CENTER);
-  image(butterflyParts.body[0].img, butterflyParts.body[0].x, butterflyParts.body[0].y, butterflyParts.body[0].img.width*bScale, butterflyParts.body[0].img.height*bScale);
+  // //imageMode(CENTER);
+  // butterflyParts.body[0].display();
+  //butterflyParts.body[0].move();
+  //image(butterflyParts.body[0].img, butterflyParts.body[0].x, butterflyParts.body[0].y, butterflyParts.body[0].img.width*bScale, butterflyParts.body[0].img.height*bScale);
 }
 
 
 
 function ButterflyPart(img, x, y, dir) {
   this.img = img;
-  this.x = x;
-  this.y = y;
-  this.startX = x;
-  this.startY = y;
+  this.finalX = windowWidth/2 + x*bScale;
+  this.finalY = windowHeight/2 + y*bScale;
   this.dir = dir;
-  this.origM = Math.sqrt((windowWidth/2 - this.x)*(windowWidth/2 - this.x)+(this.y - windowHeight/2)*(this.y - windowHeight/2));
-  this.angle = atan((this.y - windowHeight/2)/(this.x-windowWidth/2));
+  this.x = this.finalX;
+  this.y = this.finalY;
+  this.alreadyChanged = false;
   this.isSelected = false;
+  this.show = true;
   this.dragStart = {x: 0, y: 0};
-  //else this.angle = atan((this.y - windowHeight/2)/(windowWidth/2 - this.x));
-  //this.angle = radians(random(360));
+  this.lastChecked = 0;
+  this.resizePart = function(s) {
+    this.img.resize(this.img.width*s, this.img.height*s);
+  }
+  this.randomize = function() {
+    if (this.dir) {
+      this.x = random(windowWidth/10, windowWidth*9/10);
+      this.y = random(0, windowHeight/2);
+    }
+  }
+  this.resetRandom = function() {
+    this.alreadyChanged = false;
+  }
   this.display = function() {
-    // if (mode == 0) {
-    //   if (this.mouseOver()) {
-    //     imageMode(CENTER);
-    //     image(this.img, this.x, this.y, this.img.width/2, this.img.height/2);
-    //   }
-    // }
-    // else {
-      imageMode(CENTER);
-      image(this.img, this.x, this.y, this.img.width*bScale, this.img.height*bScale);
-      noFill();
-      strokeWeight(2);
-      stroke(0, width, width);
-      rect(this.x-this.img.width/2*bScale, this.y-this.img.height/2*bScale, this.img.width*bScale, this.img.height*bScale);
-    //}
-  };
+    var startT = 4;
+    var endT = 8;
+    var showT = 2;
+    var hideT = 1;
+
+
+    if (audio.currentTime > startT && (audio.currentTime < endT)) {
+      if (this.show && (millis() - this.lastChecked > showT)) {
+        console.log(3);
+        this.show = false;
+        this.lastChecked = millis();
+      }
+      else if (!this.show && (millis() - this.lastChecked > hideT)) {
+        console.log(2);
+        this.randomize();
+        this.show = true;
+        this.lastChecked = millis();
+      }
+    }
+    else if (audio.currentTime > endT) {
+      this.show = true;
+    }
+
+    if (this.show) image(this.img, this.x, this.y);
+  }
   this.mouseOver = function() {
-    //imageMode(CORNER);
-    return (mouseX > this.x-this.img.width/2*bScale && mouseX < this.x + this.img.width/2*bScale && mouseY > this.y-this.img.height/2*bScale && mouseY < this.y + this.img.height/2*bScale);
+
+    if (mouseX > this.x && mouseX < this.x + this.img.width && mouseY > this.y && mouseY < this.y + this.img.height){
+      noFill();
+      stroke(0, 255, 255);
+      ellipse(this.x + this.img.width/2, this.y+ this.img.height/2, this.img.width);
+      return true;
+    };
+    return false;
   }
   this.checkSelected = function () {
-    if (this.mouseOver() && !currSelected) {
-      this.dragStart.x = mouseX - this.x;
-      this.dragStart.y = mouseY - this.y;
-      this.isSelected = true;
-      currSelected = true;
+    if (this.mouseOver()) {
+      if (!currSelected) {
+        this.dragStart.x = mouseX - this.x;
+        this.dragStart.y = mouseY - this.y;
+        this.isSelected = true;
+        currSelected = true;
+      }
     }
   }
   this.move = function() {
@@ -148,4 +192,9 @@ function mousePressed() {
   for (var i = 0; i < butterflyParts.right.length; i++) {
     butterflyParts.right[i].checkSelected();
   }
+}
+
+window.onload = function() {
+  audio = document.getElementById('myAudio');
+  audio.play();
 }
